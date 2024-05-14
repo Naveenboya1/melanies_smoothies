cca68eeb4d90cce9b0e02d29ef1618c0ce48166b
Naveenboya1 <naveenkumar.boya@mrcooper.com>
2024-05-14 16:33:13 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -13,8 +13,6 @@ st.write(
 )
 
 
- name_on_order = st.text_input("Name of Smoothie!")
-st.write("Name of Smoothie will be", name_on_order)
 
 cnx=st.connection("snowflake")
 session = cnx.session()
@@ -22,6 +20,9 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARC
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
+
+name_on_order = st.text_input("Name of Smoothie!")
+st.write("Name of Smoothie will be", name_on_order)
 
 import streamlit as st
     
@@ -33,6 +34,7 @@ ingredients_list = st.multiselect(
 
 if  ingredients_list:
     ingredients_string=''
+    
     
     
     for fruit_choosan in ingredients_list:
