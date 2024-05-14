8a3ec845fef0755991c2cb801802bc8b67013c49
Naveenboya1 <naveenkumar.boya@mrcooper.com>
2024-05-14 15:59:22 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,7 +1,12 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+
 import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#st.text(fruityvice_response.json())
+fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie!:cup_with_straw:")
@@ -26,18 +31,18 @@ ingredients_list = st.multiselect(
     ,max_selections=5
 )   
 
-
 if  ingredients_list:
     ingredients_string=''
     
+    name_on_order = st.text_input("Name of Smoothie!")
+    st.write("Name of Smoothie will be", name_on_order)
     
-name_on_order = st.text_input("Name of Smoothie!")
-st.write("Name of Smoothie will be", name_on_order)
     
-    
-        for fruit_choosan in ingredients_list:
-           ingredients_string+=fruit_choosan +' '
-           st.write(ingredients_string)
+    for fruit_choosan in ingredients_list:
+        ingredients_string+=fruit_choosan +' '
+        
+            
+    st.write(ingredients_string)
 
 
 
@@ -52,18 +57,3 @@ st.write("Name of Smoothie will be", name_on_order)
    
     st.write(my_insert_stmt)
     st.stop()
-
-
-
-    
-
-      
-        
-
-
-
-
-
-
-
-    
