291909217307661509559f988e462691414db2f4
Naveenboya1 <naveenkumar.boya@mrcooper.com>
2024-05-14 16:02:52 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,11 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
 
 
 # Write directly to the app
@@ -40,6 +37,9 @@ if  ingredients_list:
     
     for fruit_choosan in ingredients_list:
         ingredients_string+=fruit_choosan +' '
+        st.subheader(fruit_choosan+'NUTRITION INFORMATION')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choosan)
+        fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
         
             
     st.write(ingredients_string)
